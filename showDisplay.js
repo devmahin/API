a8aa31693ec9d54e28f4c1ea81f7e94b23ad4501
devmahin <mahinhowlader123@gmail.com>
2024-02-26 23:19:50 +0600

    placeholder datashow

--- showDisplay.js
+++ showDisplay.js
@@ -9,15 +9,18 @@ function loadUsers(){
 const show = document.getElementById("show");
 
 function showFunction(data){
+
     for(let datas of data){
         // console.log(datas)
+        let div = document.createElement("div");
         const {id,title,body} = datas;
         console.log(id)
-        show.innerHTML +=   `
+        div.innerHTML +=   `
             <h1>${id}</h1>
             <h2>${title}</h2>
             <p>${body}</p>
              `
+    show.appendChild(div)
     }
 }
 
